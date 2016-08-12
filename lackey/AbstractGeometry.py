ce6a5e4efae664840d622282ce6f8900f41fb4ff
glitchassassin <jon.winsley@gmail.com>
2016-08-12 08:58:56 -0400

    Removed old class AbstractGeometry, updated keycodes

--- lackey/AbstractGeometry.py
+++
@@ -1,8 +0,0 @@
-import platform
-from .PlatformManagerWindows import PlatformManagerWindows
-#from .RegionMatching import Region
-
-if platform.system() == "Windows":
-	PlatformManager = PlatformManagerWindows() # No other input managers built yet
-else:
-	raise NotImplementedError("Pykuli v0.01 is currently only compatible with Windows.")
