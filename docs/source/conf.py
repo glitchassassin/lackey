be1b910b4e141054c5637b747d24b59abccaf914
glitchassassin <jon.winsley@gmail.com>
2016-08-16 12:33:26 -0400

    Added opencv to Mock list

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['ctypes', 'PIL', 'PIL.Image', 'PIL.ImageGrab', 'numpy']
+MOCK_MODULES = ['ctypes', 'PIL', 'PIL.Image', 'PIL.ImageGrab', 'numpy', 'cv2']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
