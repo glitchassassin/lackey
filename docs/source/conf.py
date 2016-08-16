377f62355b471575dfb26cb9e7b43bc98c079203
glitchassassin <jon.winsley@gmail.com>
2016-08-16 12:31:40 -0400

    Added numpy to Mock list

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['ctypes', 'PIL', 'PIL.Image', 'PIL.ImageGrab']
+MOCK_MODULES = ['ctypes', 'PIL', 'PIL.Image', 'PIL.ImageGrab', 'numpy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
