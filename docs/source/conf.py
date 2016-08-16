b987f34ba8a0b5d03e566d6743e974e3cecc7108
glitchassassin <jon.winsley@gmail.com>
2016-08-16 11:12:28 -0400

    Troubleshooting RTD mocking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['PlatformManagerWindows']
+MOCK_MODULES = ['ctypes']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
