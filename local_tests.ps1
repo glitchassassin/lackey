0b715d57912caf423e6cd559a88eb59d07e147d3
glitchassassin <jon.winsley@gmail.com>
2017-04-13 15:44:17 -0400

    Added coverage badge generator

--- local_tests.ps1
+++ local_tests.ps1
@@ -1,3 +1,4 @@
 python -m coverage run --source=lackey -m unittest tests.test_cases
 py -3 -m coverage run -a --source=lackey -m unittest tests.test_cases
 coverage html
+coverage-badge -o coverage.svg
