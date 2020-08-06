8f1683c82560af761bdbef6cf78a6f3ce97a66de
Jon Winsley <jon.winsley@gmail.com>
2020-08-06 13:12:12 -0400

    Fix for #150 documentation inconsistency

--- lackey/RegionMatching.py
+++ lackey/RegionMatching.py
@@ -617,7 +617,7 @@ class Region(object):
     def waitVanish(self, pattern, seconds=None):
         """ Waits until the specified pattern is not visible on screen.
 
-        If ``seconds`` pass and the pattern is still visible, raises FindFailed exception.
+        If ``seconds`` pass and the pattern is still visible, returns false.
         If Settings.SwitchToText is True, this uses OCR to search for the text in
         `pattern` if `pattern` does not correspond to an existing image path.
         """
